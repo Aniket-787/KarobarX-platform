e7f1aa665865dee465748f5feaee8da5cb8d1737
Aniket Wagh <aniketwagh878@gmail.com>
2026-03-02 08:18:16 +0530

    Product service completed.

---
+++ Product/src/routes/product.routes.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const multer = require('multer');
+const createAuthMiddleware = require('../middleware/auth.middleware');
+const { createProductValidators } = require('../validators/product.validator');
+const productController = require('../controllers/product.controller');
+
+const router = express.Router()
+
+const upload  = multer({storage: multer.memoryStorage()})
+
+//POST : /api/products/
+
+router.post(
+    '/', 
+    createAuthMiddleware(['admin','seller']), 
+    upload.array('images',5), 
+    createProductValidators, 
+    productController.createProduct 
+)
+
+//GET : /api/products/
+
+router.get('/',productController.getProducts)
+
+//PATCH : /api/products/:id
+router.patch("/:id", createAuthMiddleware([ "seller" ]), productController.updateProduct);
+
+//DELETE : /api/products/:id
+router.delete("/:id", createAuthMiddleware([ "seller" ]), productController.deleteProduct);
+
+//GET : /api/products/seller
+router.get("/seller", createAuthMiddleware([ "seller" ]), productController.getProductsBySeller);
+
+
+// GET /api/products/:id
+router.get('/:id', productController.getProductById);
+
+
+module.exports = router
